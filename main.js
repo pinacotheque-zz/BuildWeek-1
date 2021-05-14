e806a198374a655370db0a6c09e6e44d648104a1
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-05-14 13:58:41 +0100

    heart function complete

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ let heart = document.querySelector('.favourited')
 heart.addEventListener('click', function(e) {
   // heart.style.backgroundColor = 'var(--spot-green)'
   heart.style.color = heart.style.color  === 'var(--spot-green)' ? '' : 'var(--spot-green)';
+  heart.style.fontWeight = heart.style.fontWeight  === 'bold' ? '' : 'bold';
+})
+
+let hearty = document.querySelector('.add-to-favourites')
+hearty.addEventListener('click', function(e) {
+  // heart.style.backgroundColor = 'var(--spot-green)'
+  hearty.style.color = hearty.style.color  === 'var(--spot-green)' ? '' : 'var(--spot-green)';
+  hearty.style.fontWeight = hearty.style.fontWeight  === 'bold' ? '' : 'bold';
 })
